8534d0030eb7ce4d2fe5c6dbd9f221b65a6255a7
rodrigo sabino <rodrigosabinosdm@gmail.com>
2020-12-09 20:06:43 -0300

    urna

---
+++ urna/etapas.js
@@ -0,0 +1,104 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '38111',
+                nome: "Christianne Varão",
+                partido: 'PEN',
+                fotos:[
+                    {url:'cv1.jpg', legenda: 'vereador'}
+                ]
+            },
+            {
+                numero: '57980',
+                nome: "Joel Varão",
+                partido: 'AKAT',
+                fotos:[
+                    {url:'cv2.jpg', legenda: 'vereador'}
+                ]
+            },
+            {
+                numero: '32462',
+                nome: "Filho",
+                partido: 'KHN',
+                fotos:[
+                    {url:'cv3.jpg', legenda: 'vereador'}
+                ]
+            },
+            {
+                numero: '64239',
+                nome: "Dandor",
+                partido: 'PNG',
+                fotos:[
+                    {url:'cv4.jpg', legenda: 'vereador'}
+                ]
+            },
+            {
+                numero: '62159',
+                nome: "Homero do Zé Filho",
+                partido: 'FEC',
+                fotos:[
+                    {url:'cv5.jpg', legenda: 'vereador'}
+                ]
+            },
+        ]
+    },
+    {
+        titulo: 'VEREADOR',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '47',
+                nome: "Chiquinho do Adbon",
+                partido: 'KHN',
+                vice:'Arão',
+                fotos:[
+                    {url:'cp1.jpg', legenda: 'prefeito'},
+                    {url:'v1.jpg', legenda: 'vice-prefeito', small: true}
+                ]
+            },
+            {
+                numero: '62',
+                nome: "Malrinete Gralhada",
+                partido: 'AKAT',
+                vice:'Biga',
+                fotos:[
+                    {url:'cp2.jpg', legenda: 'prefeito'},
+                    {url:'v2.jpg', legenda: 'vice-prefeito', small: true}
+                ]
+            },
+            {
+                numero: '15',
+                nome: "Dr. Francisco",
+                partido: 'PNG',
+                vice:'João Rodrigues',
+                fotos:[
+                    {url:'cp3.jpg', legenda: 'prefeito'},
+                    {url:'v3.jpg', legenda: 'vice-prefeito', small: true}
+                ]
+            },
+            {
+                numero: '89',
+                nome: "Lindomar Pescador",
+                partido: 'PEN',
+                vice:'Malú',
+                fotos:[
+                    {url:'cp4.jpg', legenda: 'prefeito'},
+                    {url:'v4.jpg', legenda: 'vice-prefeito', small: true}
+                ]
+            },
+            {
+                numero: '59',
+                nome: "Zé Lopes",
+                partido: 'FEC',
+                vice:"Francisca Ferreira Ramos",
+                fotos:[
+                    {url:'cp5.jpg', legenda: 'prefeito'},
+                    {url:'v5.jpg', legenda: 'vice-prefeito', small: true}
+                ]
+            },
+        ]
+    }
+]
